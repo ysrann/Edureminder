2275b4816a77aa56784571a40c14dd792f6e19bc
ysrann <faiqyusran@gmail.com>
2025-05-16 11:44:55 +0700

    Create calendar.js

---
+++ calendar.js
@@ -0,0 +1,4 @@
+// Integrasi Google Calendar dan kalender ujian akan dikembangkan
+// Bisa pakai Google Calendar API dan OAuth2 untuk autentikasi
+
+console.log("Fitur integrasi kalender masih dalam pengembangan.");
